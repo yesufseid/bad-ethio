985272b139023f482a4868f15d7cbc060f8ed3e9
kalkidanyishak <kalkidanyishak001@gmail.com>
2026-03-01 23:03:14 +0300

    partners data with all their links names role and descriptions

---
+++ lib/partner-data.ts
@@ -0,0 +1,79 @@
+export const partnerCategories = [
+        {
+                category: "Strategic Sponsor",
+                partners: [
+                        {
+                                name: "Aleph Cloud",
+                                role: "Strategic Infrastructure Partner",
+                                description: "Supporting the technical backbone and long-term builder ecosystem growth of BlockFest Ethiopia.",
+                                logo: "https://res.cloudinary.com/dsbgn7nz8/image/upload/v1772390045/5821_l2bnpw.png",
+                                website: "https://www.aleph.cloud/"
+                        }
+                ]
+        },
+        {
+                category: "Ecosystem & Protocol Partners",
+                partners: [
+                        {
+                                name: "Africa Blockchain Institute",
+                                description: "Providing technical expertise, ecosystem access, mentorship, and structured builder pathways.",
+                                logo: "https://res.cloudinary.com/dsbgn7nz8/image/upload/v1772392196/logo_1_mcafe4.png",
+                                website: "https://africablockchain.institute/"
+                        },
+                        {
+                                name: "Build-a-DAO",
+                                description: "Providing technical expertise, ecosystem access, mentorship, and structured builder pathways.",
+                                logo: "https://ik.imagekit.io/kalkidanyishak/BaD_Main/bad-white-logo?updatedAt=1769629613228",
+                                website: "https://www.buildadao.io/"
+                        }
+                ]
+        },
+        {
+                category: "Academic & Developer Communities",
+                partners: [
+                        { name: "Addis Ababa Science and Technology University", logo: "https://ik.imagekit.io/kalkidanyishak/BaD_Ethiopia/aastu?updatedAt=1769639379105", website: "https://www.aastu.edu.et/" },
+                        { name: "Addis Ababa University", logo: "https://res.cloudinary.com/dsbgn7nz8/image/upload/v1772387713/ADDIS-ABABA-UNIVERSITY-Logo_aeziyu.png", website: "https://www.aau.edu.et/" },
+                        { name: "Adama Science and Technology University", logo: "https://res.cloudinary.com/dsbgn7nz8/image/upload/v1772387834/images_c8y7y0.png", website: "https://www.astu.edu.et/" },
+                        { name: "GDG Addis", logo: "https://res.cloudinary.com/dsbgn7nz8/image/upload/v1772387894/Copy_20of_20GDGAddis_20_20Ethiotelecom_20-_20Telebirr_20presentation_ZJnGO1h_jhxdin.png", website: "https://gdg.community.dev/gdg-addis/" },
+                        { name: "GDG AASTU", logo: "https://res.cloudinary.com/dsbgn7nz8/image/upload/v1772387971/gdsc_aastu_logo_vdkovf.jpg", website: "https://gdg.community.dev/gdg-on-campus-addis-ababa-science-and-technology-university-addis-ababa-ethiopia/" },
+                        { name: "CSEC ASTU", logo: "https://res.cloudinary.com/dsbgn7nz8/image/upload/v1772388062/FfF79fAl_400x400_tflboy.jpg", website: "https://csec-landing-page.vercel.app/" },
+                        { name: "CGI AASTU", logo: "https://res.cloudinary.com/dsbgn7nz8/image/upload/v1772388124/images_bcwrzv.jpg", website: "https://www.linkedin.com/company/cgi-aastu/?originalSubdomain=gdg-on-campus-addis-ababa-science-and-technology-university-addis-ababa-ethiopia" }
+                ]
+        },
+        {
+                category: "Community Partners",
+                partners: [
+                        { name: "Ethiopia Blockchain Week", logo: "https://ik.imagekit.io/kalkidanyishak/BaD_Main/Ethiiopia-Blockchain-Week?updatedAt=1769630567129", website: "https://etbw.online/" },
+                        { name: "Ethiopia Blockchain Network", logo: "https://res.cloudinary.com/dsbgn7nz8/image/upload/v1772391865/images_aqppna.jpg", website: "https://x.com/EBnetwork3" },
+                        { name: "ETHiopia Residency", logo: "https://ik.imagekit.io/kalkidanyishak/BaD_Ethiopia/ETHiopia%20Residency.jpg?updatedAt=1769691633379" },
+                        { name: "Kesse Events", logo: "https://res.cloudinary.com/dsbgn7nz8/image/upload/v1772392821/230134141_1923304521161382_7718793576982467601_n.jpg_t8il97.jpg", website: "https://www.instagram.com/kese_events/" },
+                        { name: "Dev3Pack", logo: "https://ik.imagekit.io/kalkidanyishak/BaD_Main/Dev3pack?updatedAt=1769629592797", website: "https://dev3pack.xyz/" },
+                        { name: "GIDA", logo: "/GIDA-05.png", website: "https://www.gida.academy/" },
+                        { name: "Web3 Ethiopia", logo: "https://ik.imagekit.io/kalkidanyishak/BaD_Main/web3-ethiopia?updatedAt=1769628826313", website: "https://x.com/Web3Ethiopia" },
+                ]
+        },
+        {
+                category: "Hospitality & Travel Partners",
+                partners: [
+                        {
+                                name: "Ethiopian Airlines",
+                                role: "Official Travel Partner",
+                                logo: "https://res.cloudinary.com/dsbgn7nz8/image/upload/v1772393151/ethiopoian-logo_cn9cw0.png",
+                                website: "https://www.ethiopianairlines.com/et"
+                        },
+                        {
+                                name: "Point Central Hotel",
+                                role: "Official Accommodation Partner",
+                                description: "Supporting speaker hospitality and event logistics during BlockFest Ethiopia.",
+                                logo: ""
+                        }
+                ]
+        },
+        {
+                category: "Media Partners",
+                partners: [
+                        { name: "Coin Gabbar", logo: "/Coin Gabbar logo-light.svg", website: "https://www.coingabbar.com/" },
+                        { name: "We Them Media", logo: "https://res.cloudinary.com/dsbgn7nz8/image/upload/v1772392946/394258263_855506199570946_5844775266856769909_n.jpg_e8jh3k.jpg", website: "https://www.instagram.com/we_them_media/?hl=en" }
+                ]
+        }
+];

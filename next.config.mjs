6d62b7c17c880371940c2edfdfcf7caa9ff990d1
kalkidanyishak <kalkidanyishak001@gmail.com>
2026-03-01 23:03:37 +0300

    working with cloudinary.com for better image loading

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,11 @@ const nextConfig = {
         hostname: 'ik.imagekit.io',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
     ],
   },
 }

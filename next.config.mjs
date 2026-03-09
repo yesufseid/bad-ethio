7bde7f285eef0735a626b8650126cd3ad0da8572
kalkidanyishak <kalkidanyishak001@gmail.com>
2026-03-09 16:42:31 +0300

    no longer experimental

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,8 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['blockfest.bad.et', '*.blockfest.bad.et'],
-    },
+   serverActions: {
+    allowedOrigins: ['blockfest.bad.et', '*.blockfest.bad.et'],
   },
   images: {
     remotePatterns: [
